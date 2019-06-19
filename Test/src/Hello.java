a6399c500f11c1874ef262a50605f4f498f6bc97
xpertmake <36837347+xpertmake@users.noreply.github.com>
2019-06-19 11:10:49 +0900

    Update Hello.java
    서버에서 변경2번째 체크

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "";
-		str = "set value from github";
+		str = "set value from github 두번째로 서버에서 변경";
 	}
 
 }
