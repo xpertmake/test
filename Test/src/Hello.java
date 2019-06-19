e5ace3f50ab29d3f2fb01c62afc8de1ae2ade031
xpertmake <36837347+xpertmake@users.noreply.github.com>
2019-06-19 11:06:24 +0900

    Update Hello.java
    github에서 변경

--- Test/src/Hello.java
+++ Test/src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "";
-		str = "set value";
+		str = "set value from github";
 	}
 
 }
